3265a1c4f39ea52d532e9b02fd7b9727ba76240a
oumaoumag <ouma.godwin10@gmail.com>
2025-05-18 22:55:33 +0300

    Add test_ExecuteBatch function to SmartWalletTest for batch execution of transactions

--- test/SmartWallet.t.sol
+++ test/SmartWallet.t.sol
@@ -55,5 +55,37 @@ contract SmartWalletTest is Test {
         wallet.execute(address(mockContract), 0.1 ether, callData);
     }
     
+    function test_ExecuteBatch() public {
+        // Create a second mock contract
+        MockContract mockContract2 = new MockContract();
+        
+        // Prepare arrays for batch execution
+        address[] memory targets = new address[](2);
+        targets[0] = address(mockContract);
+        targets[1] = address(mockContract2);
+        
+        uint256[] memory values = new uint256[](2);
+        values[0] = 0.1 ether;
+        values[1] = 0.2 ether;
+        
+        bytes[] memory callData = new bytes[](2);
+        callData[0] = abi.encodeWithSelector(MockContract.setValue.selector, 42);
+        callData[1] = abi.encodeWithSelector(MockContract.setValue.selector, 84);
+        
+        // Fund the wallet
+        vm.deal(address(wallet), 1 ether);
+        
+        // Execute batch as owner
+        vm.prank(owner);
+        wallet.executeBatch(targets, values, callData);
+        
+        // Verify results
+        assertEq(mockContract.value(), 42);
+        assertEq(mockContract2.value(), 84);
+        assertEq(address(mockContract).balance, 0.1 ether);
+        assertEq(address(mockContract2).balance, 0.2 ether);
+        assertEq(wallet.nonce(), 1);
+    }
     
+   
 }
